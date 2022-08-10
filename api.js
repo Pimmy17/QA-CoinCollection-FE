caec55ca158c74d450e18470f9a20bea4ea9231d
Andy Pimlott <andy.pimlott17@hotmail.co.uk>
2022-08-10 18:18:47 +0100

    Still working on create a coin - console logs ok but error with CORS and API

--- api.js
+++ api.js
@@ -1,46 +1,78 @@
+const coinURL = "http://localhost:8080/home";
+
 const coinEl = document.querySelector('#coins');
+const denominationEl = document.querySelector('#denomination');
 const yearEl = document.querySelector('#year');
-const thirdEl = document.querySelector('#third');
-const mainURL = "http://localhost:8080/home";
-
-//Home Page
-axios.get(`${mainURL}`).then((response) => {
-    response.data
-}).catch((err) => {
-    console.log(err)
-});
+const coinNameEl = document.querySelector('#coin_name');
+const inCollectionEl = document.querySelector('#incollection');
+const countryEl = document.querySelector('#country');
+const coinDescEl = document.querySelector('#description')
+const currentDatabaseElement = document.querySelector("#current-database");
 
 
-//Get List of Coins
-axios.get(`${mainURL}/coins`).then((response) => {
-    console.log(response.data)
-    response.data.data.forEach(e => printToScreen(e))
-}).catch((err) => {
-    console.log(err)
-});
+const readAllCoins = () => {
+    console.log('Here');
+    fetch(`${coinURL}/coins`)
+        .then(response => response.json())
+        .then(coins => {
+            
+            if (coins.length != 0) {
+                for (let i = 0; i < coins.length; i++) {
+                    
+                    let li = document.createElement("li");
+                    li.textContent = `${coins[i].coin_name} ${coins[i].year} ${coins[i].country}`;
+                    addcurrentDatabaseElement.appendChild(li);
+                }
+            } else {
+                console.log("No Coins");
+                currentDatabaseElement.innerHTML = `<p class="text-black-50 text-center"><em>No tasks to show!</em></p>`;
+            }
+
+        })
+        .catch(err => console.error(`error ${err}`));
+};
 
 
-const printToScreen = (info) => {
+
+const printToScreen = (information) => {
     const p = document.createElement("p");
-    const text = document.createTextNode(`${info}`);
+    const text = document.createTextNode(`${information.coinName} ${information.year}, ${information.denomination} ${information.country}`);
     p.appendChild(text);
     coinEl.appendChild(p);
 }
 
-const createCoin= () => {
-    const coinName = coinArray[0];
-    const denomination = denominationEl.value;
-    const year = yearEl.value;
+const createCoin = () => {
+
+    let coinName = coinNameEl.value;
+    let denomination = denominationEl.value;
+    let year = yearEl.value;
+    let country = countryEl.value;
+    let description = coinDescEl.value;
+    let inCollection = inCollectionEl.value;
+
     const coin = {
         'coin_name' : coinName,
         'denomination' : denomination,
-        'year' : year    }
-    axios        .post("https://reqres.in/api/users", user)
-        .then((res) => {
-            console.log(res)
-            printToScreen(res.data);
-        }).catch((err) => {
-            console.error(err);
+        'year' : year,
+        'incollection' : inCollection,
+        'coin_description' : description,
+        'country' : country
+        }
+
+
+        fetch(`${coinURL}/createCoin`, {
+            method: "POST",
+            body: JSON.stringify(coin),
+            headers: {
+                "Content-Type": "application/json"
+            }
+            
+        }).then(response => response.json())
+        .then(model => {
+            console.log(model);
+            readAllCoins();
         })
-    console.log(user)
+        .catch(err => console.error(`error ${err}`));
+
+    console.log(coin)
 }
