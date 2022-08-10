45f8a00df2f69cbb4e214fd0a223c007f4a2b82d
Andy Pimlott <andy.pimlott17@hotmail.co.uk>
2022-08-10 19:18:09 +0100

    Removed stray button

--- api.js
+++ api.js
@@ -10,27 +10,27 @@ const coinDescEl = document.querySelector('#description')
 const currentDatabaseElement = document.querySelector("#current-database");
 
 
-const readAllCoins = () => {
-    console.log('Here');
-    fetch(`${coinURL}/coins`)
-        .then(response => response.json())
-        .then(coins => {
+// const readAllCoins = () => {
+//     console.log('Here');
+//     fetch(`${coinURL}/coins`)
+//         .then(response => response.json())
+//         .then(coins => {
             
-            if (coins.length != 0) {
-                for (let i = 0; i < coins.length; i++) {
+//             if (coins.length != 0) {
+//                 for (let i = 0; i < coins.length; i++) {
                     
-                    let li = document.createElement("li");
-                    li.textContent = `${coins[i].coin_name} ${coins[i].year} ${coins[i].country}`;
-                    addcurrentDatabaseElement.appendChild(li);
-                }
-            } else {
-                console.log("No Coins");
-                currentDatabaseElement.innerHTML = `<p class="text-black-50 text-center"><em>No tasks to show!</em></p>`;
-            }
+//                     let li = document.createElement("li");
+//                     li.textContent = `${coins[i].coin_name} ${coins[i].year} ${coins[i].country}`;
+//                     addcurrentDatabaseElement.appendChild(li);
+//                 }
+//             } else {
+//                 console.log("No Coins");
+//                 currentDatabaseElement.innerHTML = `<p class="text-black-50 text-center"><em>No tasks to show!</em></p>`;
+//             }
 
-        })
-        .catch(err => console.error(`error ${err}`));
-};
+//         })
+//         .catch(err => console.error(`error ${err}`));
+// };
 
 
 
@@ -48,13 +48,12 @@ const createCoin = () => {
     let year = yearEl.value;
     let country = countryEl.value;
     let description = coinDescEl.value;
-    let inCollection = inCollectionEl.value;
 
     const coin = {
         'coin_name' : coinName,
         'denomination' : denomination,
         'year' : year,
-        'incollection' : inCollection,
+        'inCollection' : false,
         'coin_description' : description,
         'country' : country
         }
@@ -69,8 +68,9 @@ const createCoin = () => {
             
         }).then(response => response.json())
         .then(model => {
+            console.log('model area')
             console.log(model);
-            readAllCoins();
+            // readAllCoins();
         })
         .catch(err => console.error(`error ${err}`));
 
