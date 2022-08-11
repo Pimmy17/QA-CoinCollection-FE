b61eef4465df53df5e17e9979a4d6b63412d813e
Andy Pimlott <andy.pimlott17@hotmail.co.uk>
2022-08-11 16:41:33 +0100

    Delete button created and working. Small tweak to CSS

--- api.js
+++ api.js
@@ -21,7 +21,7 @@ function addRow(tableID) {
     let newRow = tableRef.insertRow(-1);
     let link1 = document.createElement("a");
     link1.setAttribute("href", "./individualcoin.html")
-    link1.className = "someCSSclass";
+    link1.className = "link";
     let linkText = document.createTextNode(e.coin_name);
     link1.appendChild(linkText);
   
@@ -29,6 +29,8 @@ function addRow(tableID) {
     let newCell1 = newRow.insertCell(1);
     let newCell2 = newRow.insertCell(2);
     let newCell3 = newRow.insertCell(3);
+    let newCell4 = newRow.insertCell(4);
+    let newCell5 = newRow.insertCell(5);
     let newText = document.createTextNode(e.country);
     let newText2 = document.createTextNode(e.year);
     let checkbox = document.createElement("input");
@@ -53,14 +55,43 @@ function addRow(tableID) {
         .catch(err => console.error(`error ${err}`))
     } 
     }
-
-
     checkbox.type = "checkbox";
     checkbox.addEventListener("click", addToCollection);
+
+    //Delete A Coin from Database
+    let deleteButton = document.createElement("button");
+    deleteButton.type = "button"
+    deleteButton.className = "deleteButton"
+
+    const deleteCoin = () => {
+        fetch(`${coinURL}/deleteCoin/${e.id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(response => console.log(response))
+        .then(model => {
+            console.log(model);
+        })
+        .catch(err => console.error(`error ${err}`))
+    }
+
+    deleteButton.addEventListener("click", deleteCoin);
+
+
+    //Add In Collection Flag
+        const inCollectionFlag = document.createTextNode("IN");
+        const notInCollectionFlag = document.createTextNode("");
+
+        let collectionFlag = (e.inCollection ? inCollectionFlag : notInCollectionFlag);
+    
+
     newCell0.appendChild(newText);
     newCell1.appendChild(link1);
     newCell2.appendChild(newText2);
     newCell3.appendChild(checkbox);
+    newCell4.appendChild(collectionFlag);
+    newCell5.appendChild(deleteButton);
 
   }
   addRow('coin-database');
@@ -68,7 +99,7 @@ function addRow(tableID) {
     .catch(err => 
     console.error(`error ${err}`));
 };
-readAllCoins();
+
 
 // //View an Individual Coin
 // const readCoinById = () => {
@@ -108,8 +139,9 @@ const createCoin = () => {
         }).then(response => response.json())
         .then(model => {
             console.log(model);
-            readAllCoins();
+            
         })
         .catch(err => console.error(`error ${err}`));
 }
 
+readAllCoins();
